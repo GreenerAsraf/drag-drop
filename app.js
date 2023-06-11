3e555b0103056403d53a350e63cbe4e53cc4d83f
Asraf <asraf.uddin4407@gmail.com>
2023-06-12 00:33:55 +0600

    finalized

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //DOM Elements
 const boxes = document.querySelectorAll('.box'),
   image = document.querySelector('.image')
+text = document.querySelector('.text')
 
 //Loop through each boxes element
 boxes.forEach((box) => {
@@ -15,9 +16,24 @@ boxes.forEach((box) => {
     box.classList.remove('hovered')
   })
 
-  //When a draggable element is dropped on a box elemen
+  //When a draggable element is dropped on a box element
   box.addEventListener('drop', () => {
     box.appendChild(image)
+
+    box.appendChild(text)
     box.classList.remove('hovered')
   })
+})
+function dragstart_handler(ev) {
+  // Add the target element's id to the data transfer object
+  ev.dataTransfer.setData('text/plain', ev.target.id)
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Get the element by id
+  const element = document.getElementById('p1')
+  // Add the ondragstart event listener
+  element.addEventListener('drop', dragstart_handler)
+
+  box.appendChild(element)
 })
